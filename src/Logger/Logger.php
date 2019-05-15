d2381a4b8a004b88e290c540d17e14258a0e9a84
zhosoft <zhosoft@foxmail.com>
2019-05-15 16:45:25 +0800

    update

--- src/Logger/Logger.php
+++ src/Logger/Logger.php
@@ -10,10 +10,11 @@ namespace Zhosoft;
 
 class Logger
 {
-    public $appPath = '';//日志文件路径
+    public $appPath = '';//日志文件目录
     public $fileName = '';//日志文件名称
     public $postfix = 'txt';//日志文件后缀
     const DIR_PERMISSION = '0775';//目录权限
+    const DEFAULT_APPPATH = '/../application/';//默认日志文件目录
 
     /**
      * @param $data 要写入的数据 string|array|object
@@ -22,7 +23,7 @@ class Logger
      * @param string $fileName 日志文件名称
      * @param string $postfix 日志文件后缀
      */
-    public static function writeStyle($data, $dataTag, $appPath = '', $fileName = '', $postfix = 'txt')
+    public static function writeStyle($dataTag, $data, $fileName = '', $appPath = self::DEFAULT_APPPATH, $postfix = 'txt')
     {
         //定义日志记录的目录
         $basePath = $_SERVER['DOCUMENT_ROOT'];
@@ -33,7 +34,7 @@ class Logger
         $destination = $path . date("d") . $fileName . '-log' . '.' . $postfix;
         $path = dirname($destination);
         !is_dir($path) && mkdir($path, self::DIR_PERMISSION, true);
-        file_put_contents($destination, '---------------------------------------------------------------'.PHP_EOL. date('Y-m-d H:i:s') . PHP_EOL . $dataTag . ":" . PHP_EOL . print_r($data, true) . PHP_EOL, FILE_APPEND);
+        file_put_contents($destination, '---------------------------------------------------------------' . PHP_EOL . date('Y-m-d H:i:s') . PHP_EOL . $dataTag . ":" . PHP_EOL . print_r($data, true) . PHP_EOL, FILE_APPEND);
     }
 
     /**
@@ -41,7 +42,7 @@ class Logger
      * @param $data 要写入的数据 string|array|object
      * @param $dataTag 要写入数据的描述
      */
-    public function writeStyle2($data, $dataTag)
+    public function writeStyle2($dataTag, $data)
     {
         //定义日志记录的目录
         $basePath = $_SERVER['DOCUMENT_ROOT'];
